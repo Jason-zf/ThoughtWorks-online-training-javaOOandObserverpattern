73ee3262c6326c3ac2ba6dc7d63b6d56dcb0404f
Jason <baofeng_zheng@163.com>
2018-04-09 10:48:04 +0800

    complete practice02 included class person and student

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,21 @@
 package practice02;
 
-public class Student {
+import java.awt.font.TextHitInfo;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String tom, int i, int i1) {
+        super(tom, i);
+        this.klass = i1;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class " + String.valueOf(this.klass) + ".";
+    }
 }
