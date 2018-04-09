2eb29535ef7aad7e283d8ffdf8f761a99e9e2c75
Jason <baofeng_zheng@163.com>
2018-04-09 11:31:37 +0800

    complete practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,24 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String tom, int i, int i1) {
+        super(tom, i);
+        this.klass = i1;
+    }
+
+    public Teacher(String tom, int i) {
+        super(tom, i);
+        this.klass = 0;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (this.klass == 0 ? "No Class." : "Class " + String.valueOf(this.klass) + ".");
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
 }
