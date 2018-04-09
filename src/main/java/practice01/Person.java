2296fdbe94f72307d8a80955dcac1b4df5f3f5b6
Jason <baofeng_zheng@163.com>
2018-04-09 10:31:46 +0800

    complete class person and test successfully

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,24 @@
 package practice01;
 
+import java.awt.font.TextHitInfo;
+
 public class Person {
+    private String name;
+    private int age;
+    public Person(String tom, int i) {
+        this.name=tom;
+        this.age=i;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + String.valueOf(this.age) + " years old.";
+    }
 }
