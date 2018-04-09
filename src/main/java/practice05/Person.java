6d041c849b16a9ebe71c1f2a26c61bbfdbe598e8
Jason <baofeng_zheng@163.com>
2018-04-09 11:13:29 +0800

    complete practice05

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,23 @@
 package practice05;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String tom, int i) {
+        this.name = tom;
+        this.age = i;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + String.valueOf(this.age) + " years old.";
+    }
 }
--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,19 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String tom, int i, int i1) {
+        super(tom, i);
+        this.klass = i1;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + String.valueOf(this.klass) + ".";
+    }
 }
