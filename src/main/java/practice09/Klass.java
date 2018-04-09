51e6f7201205a9e2a2aa6a55438787bca7480d3d
Jason <baofeng_zheng@163.com>
2018-04-09 14:36:12 +0800

    complete practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private boolean isInthisKlass;
+
+    public Klass(int i) {
+        number = i;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + String.valueOf(number);
+    }
+
+    public void assignLeader(Student jerry) {
+        if (jerry.getKlass().isInthisKlass == true) {
+            leader = jerry;
+            return;
+        }
+        System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student jerry) {
+        jerry.getKlass().isInthisKlass = true;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,27 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int i, String tom, int i1, Klass klass) {
+        super(i, tom, i1);
+        this.klass = klass;
+    }
+
+    public Teacher(int i, String tom, int i1) {
+        super(i, tom, i1);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (this.klass == null ? "No Class" : this.klass.getDisplayName()) + ".";
+    }
+
+    public String introduceWith(Student jerry) {
+        return super.introduce() + " I am a Teacher. I " + (this.klass.getNumber() == jerry.getKlass().getNumber() ? "teach " : "don't teach ") + jerry.getName() + ".";
+    }
 }
