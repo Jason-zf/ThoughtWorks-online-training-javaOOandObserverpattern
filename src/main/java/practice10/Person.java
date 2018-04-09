7f0cc61343ac86a69e1f75b31fb448f682b89950
Jason <baofeng_zheng@163.com>
2018-04-09 15:59:07 +0800

    complete practice10

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,35 @@
 package practice10;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int i, String tom, int i1) {
+        id = i;
+        name = tom;
+        age = i1;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + String.valueOf(age) + " years old.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (this == null||obj==null) ? false : this.id == ((Person) obj).id;
+    }
+
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,42 @@
 package practice10;
 
-public class Teacher {
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private HashSet<Klass> classes;
+
+    public <E> Teacher(int i, String tom, int i1, HashSet<Klass> es) {
+        super(i, tom, i1);
+        classes = es;
+    }
+
+    public Teacher(int i, String tom, int i1) {
+        super(i, tom, i1);
+    }
+
+    public HashSet<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        if (classes == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        List<Integer> cl = classes.stream().map(klass -> klass.getNumber()).collect(Collectors.toList());
+        Collections.sort(cl);
+        return super.introduce() + " I am a Teacher. I teach Class " + cl.stream().map(integer -> String.valueOf(integer)).collect(Collectors.joining(", ")) + ".";
+    }
+
+    public boolean isTeaching(Student jerry) {
+        return classes.stream().filter(klass -> jerry.getKlass() == klass).collect(Collectors.toSet()).size() != 0;
+    }
+
+    public String introduceWith(Student jerry) {
+        return super.introduce() + " I am a Teacher. I " + (this.classes.stream().filter(klass -> klass == jerry.getKlass()).count() != 0 ? "teach " : "don't teach ") + jerry.getName() + ".";
+    }
 }
